62ac7a082d4cd81a56f4397217f2390c8a19d0a2
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2021-04-21 02:20:27 +0530

    h

--- Website/app.js
+++ Website/app.js
@@ -12,7 +12,7 @@ var io = require('socket.io')(server)
 app.use(cors())
 app.use(bodyParser.json())
 
-if(process.env.NODE_ENV==='production'){
+if(process.env.PROD){
 	app.use(express.static(__dirname+"/build"))
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(__dirname+"/build/index.html"))
--- Website/src/serviceWorker.js
+++ Website/src/serviceWorker.js
@@ -9,7 +9,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+	if (process.env.PROD && 'serviceWorker' in navigator) {
 		
 		const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
 		if (publicUrl.origin !== window.location.origin) {
