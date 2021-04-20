cb151ff3701b12614c6191f118adeab8a86b67f4
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2021-04-21 01:48:35 +0530

    Update Video.js

--- Website/src/Video.js
+++ Website/src/Video.js
@@ -20,7 +20,7 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? ' ' : "http://localhost:4001"
+const server_url = process.env.NODE_ENV === 'production' ? ' ' : "https://v-shop-myntra.herokuapp.com/"
 
 var connections = {}
 const peerConnectionConfig = {
