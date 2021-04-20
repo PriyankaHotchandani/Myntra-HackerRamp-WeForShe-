a2205993144e44d9fc6cb3f1e22b2d3bc1f90a9a
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2021-04-21 01:34:54 +0530

    Update serviceWorker.js

--- Website/src/serviceWorker.js
+++ Website/src/serviceWorker.js
@@ -1,10 +1,11 @@
 
 const isLocalhost = Boolean(
-	window.location.hostname === 'localhost' ||
-	window.location.hostname === '[::1]' ||
-	window.location.hostname.match(
-		/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-	)
+	// window.location.hostname === 'localhost' ||
+	// window.location.hostname === '[::1]' ||
+	// window.location.hostname.match(
+	// 	/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+	// )
+	window.location.hostname === 'https://v-shop-myntra.herokuapp.com/'
 );
 
 export function register(config) {
